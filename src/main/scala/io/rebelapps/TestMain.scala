a9529f32251da33df2f57b56324c40a0d56b3bb2
lolczak <lukasz.jan.olczak@gmail.com>
2019-07-23 21:35:32 +0200

    showcase

--- src/main/scala/io/rebelapps/TestMain.scala
+++ src/main/scala/io/rebelapps/TestMain.scala
@@ -20,25 +20,25 @@ object TestMain extends App {
       result   <- eval { next2 + 3 }
     } yield result
 
-//  val fiber2 =
-//    for {
-//      _       <- spawn { effect { println("spawned2") }  }
-//      value   <- pure { 23 }
-//      next     = value + 1
-//      next2   <- eval { next + 3 }
-//      result  <- async[Int] { cb => new Thread(() => { cb(Right(next2+1)) }).start() }
-//    } yield result
+  val fiber2 =
+    for {
+      _       <- spawn { effect { println("spawned2") }  }
+      value   <- pure { 23 }
+      next     = value + 1
+      next2   <- eval { next + 3 }
+      result  <- async[Int] { cb => new Thread(() => { cb(Right(next2+1)) }).start() }
+    } yield result
 
   println(fiber1)
 
   val future1 = Scheduler.run(fiber1 map (_ + 5))
-//  val future2 = Scheduler.run(fiber2 map(_ + 5))
+  val future2 = Scheduler.run(fiber2 map(_ + 5))
 
   val result1 = Await.result(future1, 10 seconds)
-//  val result2 = Await.result(future2, 10 seconds)
+  val result2 = Await.result(future2, 10 seconds)
 
   println(result1)
-//  println(result2)
+  println(result2)
 
   Scheduler.shutdown()
 
