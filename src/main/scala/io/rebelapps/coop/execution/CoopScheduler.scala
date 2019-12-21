2c420c76d3667d2a6c785d4de9a0e1992300b8ae
lolczak <lukasz.jan.olczak@gmail.com>
2019-12-21 21:11:46 +0100

    refactoring

--- src/main/scala/io/rebelapps/coop/execution/CoopScheduler.scala
+++ src/main/scala/io/rebelapps/coop/execution/CoopScheduler.scala
@@ -49,31 +49,42 @@ object CoopScheduler {
 
           case Suspended(go, deferred) =>
             val requestId = UUID.randomUUID()
-            runtimeCtxRef.update(_.removeRunning(fiber))
-            runtimeCtxRef.update(_.addSuspended(requestId, fiber))
+            runtimeCtxRef.update { ctx =>
+              ctx
+                .removeRunning(fiber)
+                .addSuspended(requestId, fiber)
+            }
             go {
               case Left(ex)      => throw ex
               case Right(result) =>
                 pool.execute { () =>
-                  val fiber = runtimeCtxRef.modify_(_.removeSuspended(requestId))
-                  runtimeCtxRef.update(_.enqueueReady(fiber))
+                  runtimeCtxRef.update { ctx =>
+                    val (updated, fiber) = ctx.removeSuspended(requestId)
+                    updated.enqueueReady(fiber)
+                  }
                   deferred.fill(result)
                   tryAwakeOneCoroutine()
                 }
             }
 
           case CreateFiber(coroutine) =>
-            runtimeCtxRef.update(_.removeRunning(fiber))
-            runtimeCtxRef.update(_.enqueueReady(fiber))
+            runtimeCtxRef.update { ctx =>
+              ctx
+                .removeRunning(fiber)
+                .enqueueReady(fiber)
+            }
             run(coroutine)
             tryAwakeOneCoroutine() //wakes creator
 
           case ChannelCreation(size, deferred) =>
             val id = UUID.randomUUID()
             val channel = new SimpleChannel[Any](id, size)
-            runtimeCtxRef.update(_.upsertChannel(id, channel))
-            runtimeCtxRef.update(_.removeRunning(fiber))
-            runtimeCtxRef.update(_.enqueueReady(fiber))
+            runtimeCtxRef.update { ctx =>
+              ctx
+                .upsertChannel(id, channel)
+                .removeRunning(fiber)
+                .enqueueReady(fiber)
+            }
             deferred.fill(channel)
             tryAwakeOneCoroutine() //wakes channel creator
 
@@ -81,53 +92,74 @@ object CoopScheduler {
             val channel = runtimeCtxRef.get().getChannel(id)
             if (channel.queue.isEmpty && channel.writeWait.nonEmpty) {
               val (ch, (wFiber, wElem)) = channel.getFirstWaitingForWrite()
-              runtimeCtxRef.update(_.upsertChannel(channel.id, ch))
-              runtimeCtxRef.update(_.enqueueReady(wFiber))
               deferred.fill(wElem)
-              runtimeCtxRef.update(_.removeRunning(fiber))
-              runtimeCtxRef.update(_.enqueueReady(fiber))
+              runtimeCtxRef.update { ctx =>
+                ctx
+                  .upsertChannel(channel.id, ch)
+                  .enqueueReady(wFiber)
+                  .removeRunning(fiber)
+                  .enqueueReady(fiber)
+              }
               tryAwakeOneCoroutine() //wakes reader
               tryAwakeOneCoroutine() //wakes writer
             } else if (channel.queue.nonEmpty) {
               val (ch, elem) = channel.dequeue()
-              runtimeCtxRef.update(_.upsertChannel(channel.id, ch))
               deferred.fill(elem)
-              runtimeCtxRef.update(_.removeRunning(fiber))
-              runtimeCtxRef.update(_.enqueueReady(fiber))
+              runtimeCtxRef.update { ctx =>
+                ctx
+                  .upsertChannel(channel.id, ch)
+                  .removeRunning(fiber)
+                  .enqueueReady(fiber)
+              }
               if (channel.writeWait.nonEmpty) {
                 val (ch2, (wFiber, wElem)) = ch.getFirstWaitingForWrite()
                 val currentChannel = ch2.enqueue(wElem)
-                runtimeCtxRef.update(_.upsertChannel(channel.id, currentChannel))
-                runtimeCtxRef.update(_.enqueueReady(wFiber))
+                runtimeCtxRef.update { ctx =>
+                  ctx
+                    .upsertChannel(channel.id, currentChannel)
+                    .enqueueReady(wFiber)
+                }
                 tryAwakeOneCoroutine() //wakes writer
               }
               tryAwakeOneCoroutine() //wakes trader
             } else {
-              runtimeCtxRef.update(_.removeRunning(fiber))
               val ch = channel.waitForRead(fiber, deferred)
-              runtimeCtxRef.update(_.upsertChannel(channel.id, ch))
+              runtimeCtxRef.update { ctx =>
+                ctx
+                  .upsertChannel(channel.id, ch)
+                  .removeRunning(fiber)
+              }
               tryAwakeOneCoroutine() //wakes writer
             }
 
           case ChannelWrite(id, elem) =>
             val channel = runtimeCtxRef.get().getChannel(id)
             if (channel.readWait.nonEmpty) {
-              runtimeCtxRef.update(_.removeRunning(fiber))
-              runtimeCtxRef.update(_.enqueueReady(fiber))
               val (ch, (f, defVal)) = channel.getFirstWaitingForRead()
-              runtimeCtxRef.update(_.upsertChannel(channel.id, ch))
               defVal.fill(elem)
-              runtimeCtxRef.update(_.enqueueReady(f))
+              runtimeCtxRef.update { ctx =>
+                ctx
+                  .removeRunning(fiber)
+                  .enqueueReady(fiber)
+                  .upsertChannel(channel.id, ch)
+                  .enqueueReady(f)
+              }
             } else {
               if (channel.queue.size < channel.queueLength) {
                 val currentChannel = channel.enqueue(elem)
-                runtimeCtxRef.update(_.upsertChannel(channel.id, currentChannel))
-                runtimeCtxRef.update(_.removeRunning(fiber))
-                runtimeCtxRef.update(_.enqueueReady(fiber))
+                runtimeCtxRef.update { ctx =>
+                  ctx
+                    .upsertChannel(channel.id, currentChannel)
+                    .removeRunning(fiber)
+                    .enqueueReady(fiber)
+                }
               } else {
                 val currentChannel = channel.waitForWrite(elem, fiber)
-                runtimeCtxRef.update(_.upsertChannel(channel.id, currentChannel))
-                runtimeCtxRef.update(_.removeRunning(fiber))
+                runtimeCtxRef.update { ctx =>
+                  ctx
+                    .upsertChannel(channel.id, currentChannel)
+                    .removeRunning(fiber)
+                }
               }
             }
             tryAwakeOneCoroutine()//wakes reader
