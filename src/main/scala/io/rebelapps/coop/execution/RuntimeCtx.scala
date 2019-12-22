233650454f76e72701ad1f35adf8ba8783f89fd6
lolczak <lukasz.jan.olczak@gmail.com>
2019-12-22 12:11:23 +0100

    queue introduced

--- src/main/scala/io/rebelapps/coop/execution/RuntimeCtx.scala
+++ src/main/scala/io/rebelapps/coop/execution/RuntimeCtx.scala
@@ -2,20 +2,22 @@ package io.rebelapps.coop.execution
 
 import java.util.UUID
 
+import scala.collection.immutable.Queue
+
 case class RuntimeCtx(running: Vector[Fiber[Any]] = Vector.empty,
-                      ready: Vector[Fiber[Any]] = Vector.empty,
+                      ready: Queue[Fiber[Any]] = Queue.empty,
                       suspended: Map[UUID, Fiber[Any]] = Map.empty,
                       channels: Map[UUID, SimpleChannel[Any]] = Map.empty) {
 
   def enqueueReady(fiber: Fiber[Any]): RuntimeCtx = {
-    this.copy(ready = fiber +: ready)
+    this.copy(ready = ready.enqueue(fiber))
   }
 
   def hasReadyFibers(): Boolean = ready.nonEmpty
 
   def moveFirstReadyToRunning(): (RuntimeCtx, Fiber[Any]) = {
-    val fiber = ready.last
-    this.copy(ready = ready.init, running = fiber +: running) -> fiber
+    val (fiber, queue) = ready.dequeue
+    this.copy(ready = queue, running = fiber +: running) -> fiber
   }
 
   def removeRunning(fiber: Fiber[_]): RuntimeCtx = this.copy(running = running.filterNot(_ eq fiber))
